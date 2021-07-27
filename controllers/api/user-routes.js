495969b0aaa03c36261db3ac0b6f5e49c804d26a
zoomzoom6 <tonyhuang107@live.ca>
2021-07-27 16:27:04 -0400

    create controller folder and express server config files

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+//GET /api/users
+router.get('/', (req, res) => {
+
+});
+
+//GET /api/users/:id
+router.get('/:id', (req, res) => {
+
+});
+
+//POST /api/users
+router.post('/', (req, res) => {
+
+});
+
+//POST /api/users/login
+router.post('/login', (req, res) => {
+
+});
+
+//POST /api/users/logout
+router.post('/logout', (req, res) => {
+
+});
+
+//PUT /api/users/:id
+router.put('/:id', (req, res) => {
+
+});
+
+//DELETE /api/users/:id
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+//replace this when/if we find a different templating engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session=sequelize')(session.Store);
+
+const sess = {
+    secret: 'Ultra secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(routes);
+
+//replace this when/if we find a different templating engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
