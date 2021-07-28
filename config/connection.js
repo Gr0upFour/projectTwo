0bab0b76238aed3877a1e8327c0c5c4f8fc77dd5
zoomzoom6 <tonyhuang107@live.ca>
2021-07-28 15:26:45 -0400

    connect to jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,15 @@ require('dotenv').config();
 //DB_NAME = 'database_name'
 //DB_USER = 'user_account_name'
 //DB_PW = 'user_account_password'
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 module.exports = sequelize;
