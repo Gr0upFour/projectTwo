93eea256fa94121591536b14ed2ffee70ee1855e
zoomzoom6 <tonyhuang107@live.ca>
2021-08-10 18:53:13 -0400

    fix local sequelize declaration

--- config/connection.js
+++ config/connection.js
@@ -12,8 +12,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-//    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    sequelize = new Sequelize('window_shopper', 'root', 'mysql', {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {   
         host: 'localhost',
         dialect: 'mysql',
         port: 3306,
